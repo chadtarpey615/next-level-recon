375048bfe9b8d8bc25f6e346f092c62390200650
Chad Tarpey <chadtarpey@gmail.com>
2021-07-07 16:36:11 -0500

    installed fontawesome for navbar icon

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaAlignJustify } from "react-icons/fa";
 
 import "./Navbar.css"
 const Navbar = () => {
@@ -9,6 +10,7 @@ const Navbar = () => {
             <div className="nav-header">
                 <h1>Next-Level-Recon</h1>
             </div>
+            <FaAlignJustify className="mobile" />
             <a href="/">Home</a>
             <a href="/about">About</a>
             <a href="/contact">Contact</a>
