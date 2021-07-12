b92a520cd88e98124c387fa985b721efa011bbc6
Chad Tarpey <chadtarpey@gmail.com>
2021-07-12 12:00:29 -0500

    installed react/bootstrao

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,22 +1,45 @@
 import React from 'react';
+import { Navbar } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
 import { FaAlignJustify } from "react-icons/fa";
+import { Form } from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
 
 import "./Navbar.css"
-const Navbar = () => {
+const Navbars = () => {
     return (
 
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Next Level Recon </Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-info">Search</Button>
+                </Form>
+            </Navbar>
 
-        <div className="navbar">
-            <div className="nav-header">
-                <h1>Next-Level-Recon</h1>
-            </div>
-            <FaAlignJustify className="mobile" />
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
-        </div>
+        </>
+
 
     )
 }
 
-export default Navbar;
+export default Navbars;
+
+
+<div className="navbar">
+    <div className="nav-header">
+        <h1>Next-Level-Recon</h1>
+    </div>
+    <FaAlignJustify className="mobile" />
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+</div>
